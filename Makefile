e478e732f740cb3f67424449d0a7ba718fa50ad7
edii <edii87shadow@gmail.com>
2020-09-11 08:36:40 +0300

    clear make file

--- Makefile
+++ Makefile
@@ -1,10 +1,26 @@
-run:
+cognito-run:
 	PORT=8085 \
 	HOST=localhost \
 	AUTH_SERVER_URL=https://poc-dev.auth.eu-west-1.amazoncognito.com \
 	AUTH_TYPE=cognito \
 	DEBUG=true \
 	TRACING_DEBUG=true \
+	JAEGER_SERVICE_NAME=traefik-tower \
+	JAEGER_SAMPLER_TYPE=const \
+	JAEGER_SAMPLER_PARAM=1 \
+	JAEGER_REPORTER_LOG_SPANS=true \
+	JAEGER_AGENT_HOST=localhost \
+	JAEGER_AGENT_PORT=6831 go run main.go
+
+cognito-aws-run:
+	PORT=8085 \
+	HOST=localhost \
+	AUTH_TYPE=cognito-aws \
+	DEBUG=true \
+	TRACING_DEBUG=true \
+	AWS_PROFILE=rbi-eks \
+	COGNITO_APP_CLIENT_ID=--client-id-- \
+	COGNITO_USER_POOL_ID=--pool-id-- \
 	JAEGER_SERVICE_NAME=traefik-tower \
 	JAEGER_SAMPLER_TYPE=const \
 	JAEGER_SAMPLER_PARAM=1 \
--- config/config.go
+++ config/config.go
@@ -5,12 +5,25 @@ import (
 )
 
 type Config struct {
-	Port          string `env:"PORT" envDefault:"8000"`
-	Host          string `env:"HOST" envDefault:"0.0.0.0"`
-	AuthServerURL string `env:"AUTH_SERVER_URL"`
-	AuthType      string `env:"AUTH_TYPE"`
-	Debug         bool   `env:"DEBUG"`
-	TracingDebug  string `env:"TRACING_DEBUG"`
+	Port               string `env:"PORT" envDefault:"8000"`
+	Host               string `env:"HOST" envDefault:"0.0.0.0"`
+	AuthServerURL      string `env:"AUTH_SERVER_URL" envDefault:""`
+	AuthType           string `env:"AUTH_TYPE"`
+	AwsRegion          string `env:"AWS_REGION" envDefault:"eu-west-1"`
+	AwsProfile         string `env:"AWS_PROFILE" envDefault:""`
+	AwsUseContext      bool   `env:"AWS_USE_CONTEXT" envDefault:"true"`
+	CognitoAppClientID string `env:"COGNITO_APP_CLIENT_ID" envDefault:""`
+	CognitoUserPoolID  string `env:"COGNITO_USER_POOL_ID" envDefault:""`
+	Debug              bool   `env:"DEBUG"`
+	TracingDebug       string `env:"TRACING_DEBUG"`
+}
+
+func (c *Config) IsAuthServiceURL() bool {
+	return c.CognitoAppClientID == "" && c.CognitoUserPoolID == ""
+}
+
+func (c *Config) IsAWSContext() bool {
+	return c.AwsUseContext
 }
 
 func FromEnv() (*Config, error) {
